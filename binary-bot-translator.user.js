a70cbfa60a9704f49fbfbeb7286466a24bfb3625
Josh Parkker <jparker@sherpas.ai>
2024-04-21 11:44:52 -0600

    finish the binary bot quick translate with undo

--- binary-bot-translator.user.js
+++ binary-bot-translator.user.js
@@ -5,55 +5,69 @@
 // @description  instantly decipher binary bot posts
 // @author       Josh Parker
 // @match        https://www.butterflies.ai/users/binary_bot_101
+// @match        https://www.butterflies.ai/users/binary_bot_101/p/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=butterflies.ai
 // @grant        none
 // ==/UserScript==
 
 (function binaryBotTranslator() {
-    const bodyMo = new MutationObserver((_, mutationObserver) => {
-        const postContainer = document.querySelector('[class="sm:mt-0"]');
-        if (postContainer) {
-            console.log(postContainer, postContainer.outerHTML);
-
-            const mo = new MutationObserver((_, captionMutationObserver) => {
-                captionMutationObserver.disconnect();
-                setTimeout(() => captionMutationObserver.observe(postContainer, {childList: true, subtree: true}), 1000);
-
-                const captionSelector = '.mb-2 > [class^=text]';
-                [...document.querySelectorAll(captionSelector)]
-                    .forEach((caption) => {
-                        console.log(caption, caption.outerHTML);
-                        const span = document.createElement('span');
-                        [...caption.childNodes].filter(e => e.nodeName === '#text').forEach(node => span.appendChild(node));
-                        console.log(span, span.outerHTML);
-                        caption.appendChild(span);
-                    });
-            });
-
-            mo.observe(postContainer, { childList: true, subtree: true });
-            mutationObserver.disconnect();
-        }
-    });
-
-    bodyMo.observe(document.body, { childList: true, subtree: true });
-    const postContainer = document.querySelector('[class="sm:mt-0"]');
-
     const clickEventListener = ({ target }) => {
-        target.textContent = target.textContent.replace(/[01]{8}( ?[01]{8})*/, target.textContent.match(/[01]{8}/g).map((e) => (
-            String.fromCharCode(Number.parseInt(e, 2))
-        )).join(''));
+        const targetChildNodes = [...target.childNodes];
+        target.innerHTML = '';
+        target.targetChildNodes = targetChildNodes;
+
+        targetChildNodes.forEach((node, i) => {
+            if (node.nodeName === '#text') {
+                const nodeText = node.textContent;
+                if (
+                    nodeText === ' '
+                    && targetChildNodes[i - 1]?.nodeName === '#text'
+                    && targetChildNodes[i + 1]?.nodeName === '#text'
+                    && targetChildNodes[i - 1]?.textContent.match(/[01]{8}/)
+                    && targetChildNodes[i + 1]?.textContent.match(/[01]{8}/)
+                ) return;
+                console.log(nodeText);
+                target.appendChild(
+                    new Text(
+                        nodeText.replace(/[01]{8}( ?[01]{8})*/, nodeText.match(/[01]{8}/g)?.map((e) => (
+                            String.fromCharCode(Number.parseInt(e, 2))
+                        ))?.join('')) || nodeText,
+                    ),
+                );
+            } else {
+                target.appendChild(node);
+            }
+        });
     };
+
+    const undoListener = ({ target }) => {
+
+
+        console.log(target.targetChildNodes);
+        if (target.targetChildNodes) {
+            // target.childNodes = target.targetChildNodes;
+            target.innerHTML = '';
+            target.targetChildNodes.forEach((node) => target.appendChild(node));
+        }
+    }
 
     document.addEventListener('keydown', ({ code }) => {
         if (code === 'KeyB') {
             document.body.addEventListener('click', clickEventListener);
         }
 
+        if (code === 'KeyZ') {
+            document.body.addEventListener('click', undoListener);
+        }
     });
 
     document.addEventListener('keyup', ({ code }) => {
         if (code === 'KeyB') {
             document.body.removeEventListener('click', clickEventListener);
         }
-    })
+
+        if (code === 'KeyZ') {
+            document.body.removeEventListener('click', undoListener);
+        }
+    });
 }());
