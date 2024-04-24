6cebd31b37284ceacb0a39bc364a57473764f55f
Josh Parkker <jparker@sherpas.ai>
2024-04-24 12:30:29 -0600

    update bundle publish and remove some console logs

--- bundle-publish.js
+++ bundle-publish.js
@@ -19,8 +19,6 @@ import('@octokit/core').then(({ Octokit }) => {
    const gistIdMatch = fileContentString.match(/downloadURL.*silly-internet-tricks\/(?<gistId>[^/]*)/);
    const gistId = gistIdMatch?.groups?.gistId;
    if (gistIdMatch && gistId) {
-    console.log(fileContentString);
-    console.log(gistId);
     const filename = file.includes('meta')
      ? fileContentString.match(/updateURL.*raw\/([^/]*)/)[1].trim()
      : fileContentString.match(/downloadURL.*raw\/([^/]*)/)[1].trim();
@@ -37,8 +35,6 @@ import('@octokit/core').then(({ Octokit }) => {
     requestOptions.files[filename] = { filename, content: fileContentString };
    }
   })).then(() => {
-   console.log(JSON.stringify(octokitRequestsOptions));
-
    octokitRequestsOptions.forEach(async (requestOptions) => {
     const oldGist = await octokit.request(`GET /gists/${requestOptions.gist_id}`, {
      gist_id: requestOptions.gist_id,
@@ -47,14 +43,12 @@ import('@octokit/core').then(({ Octokit }) => {
      },
     });
 
-    console.log(oldGist.data.files);
     const oldFile = Object.entries(oldGist.data.files).find(([fileName]) => fileName.endsWith('user.js'))[1];
-    console.log(oldFile);
 
     const delimiter = '==/UserScript==\n';
     try {
      const oldUserscriptCode = oldFile.content.split(delimiter)[1];
-     const newUserscriptCode = Object.entries(requestOptions.files).find(([fileName]) => fileName.endsWith('user.js')).content.split(delimiter)[1];
+     const newUserscriptCode = Object.entries(requestOptions.files).find(([fileName]) => fileName.endsWith('user.js'))[1].content.split(delimiter)[1];
 
      if (oldUserscriptCode !== newUserscriptCode) {
       Object.values(requestOptions.files).forEach((file) => {
