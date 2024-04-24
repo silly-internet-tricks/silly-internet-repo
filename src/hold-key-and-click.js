2b25fc82cd39e0ac4af5d3b3be54ded010e139e0
Josh Parkker <jparker@sherpas.ai>
2024-04-23 18:13:35 -0600

    mess with the highlighting technique (doesn't actually work right now)

--- src/hold-key-and-click.js
+++ src/hold-key-and-click.js
@@ -1,8 +1,46 @@
+const preventDefaultHandler = (event) => {
+ event.preventDefault();
+};
+
+const myTruthyValue = 'my-truth-value';
+
+let hoverTarget;
+
+const resetHighlight = function resetHighlight(element) {
+ console.log(element);
+ if (element?.oldBoxShadow) {
+  element.style?.removeProperty('box-shadow');
+
+  if (element.oldBoxShadow !== myTruthyValue) {
+   element.style?.setProperty('box-shadow', element.oldBoxShadow);
+  }
+
+  delete element.oldBoxShadow;
+ }
+};
+
+document.addEventListener('mouseover', ({ target }) => {
+ resetHighlight(hoverTarget);
+ hoverTarget = target;
+});
+
+const addHighlight = function addHighlight(element) {
+ element.oldBoxShadow = element.style.getPropertyValue('box-shadow') || myTruthyValue;
+ element.style.setProperty('box-shadow', '0 0 4px chartreuse');
+};
+
+const highlightWithBoxShadow = () => {
+ addHighlight(hoverTarget);
+};
+
 export default function holdKeyAndClick(handlers) {
  document.addEventListener('keydown', ({ code }) => {
   Object.entries(handlers).forEach(([key, handler]) => {
    if (code === `Key${key.toLocaleUpperCase()}`) {
+    addHighlight(hoverTarget);
     document.addEventListener('click', handler);
+    document.addEventListener('click', preventDefaultHandler);
+    document.addEventListener('mouseover', highlightWithBoxShadow);
    }
   });
  });
@@ -10,7 +48,10 @@ export default function holdKeyAndClick(handlers) {
  document.addEventListener('keyup', ({ code }) => {
   Object.entries(handlers).forEach(([key, handler]) => {
    if (code === `Key${key.toLocaleUpperCase()}`) {
+    resetHighlight(hoverTarget);
     document.removeEventListener('click', handler);
+    document.removeEventListener('click', preventDefaultHandler);
+    document.removeEventListener('mouseover', highlightWithBoxShadow);
    }
   });
  });
