b3398ad43835150d3ee475a73d627dfe53afa46e
Josh Parkker <jparker@sherpas.ai>
2024-04-24 10:40:20 -0600

    try to handle multiple gists

--- bundle-publish.js
+++ bundle-publish.js
@@ -2,7 +2,6 @@ const { glob } = require('glob');
 const { readFile } = require('node:fs/promises');
 
 const auth = process.env.PERSONAL_ACCESS_TOKEN;
-// console.log('access token length', auth.length);
 // eslint-disable-next-line import/no-extraneous-dependencies, no-shadow
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
@@ -12,7 +11,8 @@ import('@octokit/core').then(({ Octokit }) => {
 
  (async () => {
   const files = await glob('./@(dist)/**/*.@(user|meta).js');
-  const octokitRequestOptions = { files: {} };
+
+  const octokitRequestsOptions = [];
   Promise.all(files.map(async (file) => {
    const fileContent = await readFile(file);
    const fileContentString = fileContent.toString();
@@ -21,17 +21,27 @@ import('@octokit/core').then(({ Octokit }) => {
    if (gistIdMatch && gistId) {
     console.log(fileContentString);
     console.log(gistId);
-    octokitRequestOptions.gist_id = gistId;
-    octokitRequestOptions.description = fileContentString.match(/description\s+(?<description>.*)/).groups.description;
     const filename = file.includes('meta')
      ? fileContentString.match(/updateURL.*raw\/([^/]*)/)[1].trim()
      : fileContentString.match(/downloadURL.*raw\/([^/]*)/)[1].trim();
-    octokitRequestOptions.files[filename] = { filename, content: fileContentString };
+
+    // find the requestOptions in the array if they're there.
+    const requestOptions = octokitRequestsOptions.find((e) => e.gist_id === gistId)
+    || (
+     octokitRequestsOptions.push({})
+      && octokitRequestsOptions[octokitRequestsOptions.length - 1]
+    );
+
+    requestOptions.gist_id = gistId;
+    requestOptions.description = fileContentString.match(/description\s+(?<description>.*)/).groups.description;
+    requestOptions.files[filename] = { filename, content: fileContentString };
    }
   })).then(() => {
-   console.log(JSON.stringify(octokitRequestOptions));
+   console.log(JSON.stringify(octokitRequestsOptions));
 
-   octokit.request(`PATCH /gists/${octokitRequestOptions.gist_id}`, octokitRequestOptions);
+   octokitRequestsOptions.forEach((requestOptions) => {
+    octokit.request(`PATCH /gists/${requestOptions.gist_id}`, requestOptions);
+   });
   });
  })();
 });
