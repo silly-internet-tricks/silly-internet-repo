239c70aceaaaa7e12792d7535113af66854a6a8b
Josh Parkker <jparker@sherpas.ai>
2024-04-25 14:54:24 -0600

    add the make carousel function

---
+++ src/make-carousel.js
@@ -0,0 +1,29 @@
+import insertCSS from './insert-css';
+
+export default function makeCarousel(parent, children) {
+ children.forEach((child) => {
+  child.classList.add('carousel-0');
+  parent.appendChild(child);
+ });
+
+ // TODO: note the fact that the parent is required to have an id.
+ // TODO: also note the fact that the children are required to be passed in with no classes.
+ // TODO: find the right calculation for the transform amount.
+ insertCSS(`${children.map((_, i) => `
+  #${parent.id} > *.carousel-${i} {
+       transform: translateX(-${180 * i}px);
+  }
+         `).join('\n\n')}
+
+#${parent.id} > * {
+     transition: transform 0.5s;
+}
+         `);
+
+ parent.addEventListener('click', () => {
+  const carouselNumber = Number(children[0].classList[0].match(/(\d+)$/)[1]);
+  children.forEach((child) => {
+   child.classList.replace(child.classList[0], `carousel-${(1 + carouselNumber) % children.length}`);
+  });
+ });
+}
