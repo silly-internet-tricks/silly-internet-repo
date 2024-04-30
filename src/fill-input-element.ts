fdbbc78cda27f2dc1a1959123338a59640bd1281
Josh Parker <JoshWolfParker@gmail.com>
2024-04-29 19:18:00 -0600

    update the fill input element function to append to the element value instead of overwriting it

--- src/fill-input-element.ts
+++ src/fill-input-element.ts
@@ -1,13 +1,17 @@
 // this is taken from https://stackoverflow.com/a/53797269
 
-export default function fillInputElement(element: HTMLElement, value: string) {
+export default function fillInputElement(element: HTMLInputElement, value: string) {
+ const previousValue: string = element.value;
+ const newValue: string = previousValue + value;
  const valueSetter: (e: string) => void = Object.getOwnPropertyDescriptor(element, 'value').set;
  const prototypeValueSetter: (e: string) => void = Object.getOwnPropertyDescriptor(Object.getPrototypeOf(element), 'value').set;
 
  if (valueSetter !== prototypeValueSetter) {
-  prototypeValueSetter.call(element, value);
+  prototypeValueSetter.call(element, newValue);
  } else {
-  valueSetter.call(element, value);
+  // TODO: I want to check whether this can ever actually be called
+  // the null check for valueSetter in the original code on stackoverflow looked questionable to me
+  valueSetter.call(element, newValue);
  }
 
  element.dispatchEvent(new Event('input', { bubbles: true }));
