4e60ef0306a248ae8fd68fc10e8a1e4f7b562a37
Josh Parker <JoshWolfParker@gmail.com>
2024-05-01 20:36:40 -0600

    add the correct gist id for the bing script

--- src/userscripts/ollama/chat-between-bing-and-ollama.user.ts
+++ src/userscripts/ollama/chat-between-bing-and-ollama.user.ts
@@ -9,8 +9,8 @@
 // @grant        GM_xmlhttpRequest
 // @connect      localhost
 // @source       https://github.com/silly-internet-tricks/silly-internet-repo/blob/main/src/chat-between-bing-and-ollama.user.ts
-// @downloadURL  https://gist.githubusercontent.com/silly-internet-tricks/658d6cbf776a950fce06f1fa49b2fd58/raw/chat-between-bing-and-ollama.user.js
-// @updateURL    https://gist.githubusercontent.com/silly-internet-tricks/658d6cbf776a950fce06f1fa49b2fd58/raw/chat-between-bing-and-ollama.meta.js
+// @downloadURL  https://gist.githubusercontent.com/silly-internet-tricks/6d6e469a6a76c7b1270120d5db7a0145/raw/chat-between-bing-and-ollama.user.js
+// @updateURL    https://gist.githubusercontent.com/silly-internet-tricks/6d6e469a6a76c7b1270120d5db7a0145/raw/chat-between-bing-and-ollama.meta.js
 // ==/UserScript==
 
 import chatBetweenXAndSelectedOllamaModel from '../../lib/ollama/chat-between-x-and-selected-ollama-model';
