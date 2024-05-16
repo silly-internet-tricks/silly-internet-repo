21ee7b0531475be552dd2b331da67b8034c270da
Josh Parker <JoshWolfParker@gmail.com>
2024-05-15 20:27:09 -0600

    fix the bad logic for image preprocessor in ci

--- image-preprocessor.js
+++ image-preprocessor.js
@@ -1,19 +1,24 @@
 const { readFile, writeFile, access } = require('node:fs/promises');
 const { glob } = require('glob');
 
-const processImages = async function processImages() {
- // Please note that only webp is handled. Add other types as needed.
- const webps = await glob('./assets/**/*.webp');
- webps.forEach(async (webp) => {
-  const tsFilename = webp.replace(/webp$/, 'ts');
-  try {
-   // NOTE: This means that to update the image, the ts file will have to be deleted
-   access(tsFilename);
-  } catch (e) {
-   if (process.env.CI) {
-    // stub file for use in github actions
-    writeFile(tsFilename, 'export default \'\';\n');
-   } else {
+if (process.env.CI) {
+ // stub files for use in github actions
+ const fileNames = ['./assets/jeff.ts'];
+
+ fileNames.forEach((fileName) => {
+  console.log(`writing stub file for ci: ${fileName}`);
+  writeFile(fileName, "export default '';\n");
+ });
+} else {
+ const processImages = async function processImages() {
+  // Please note that only webp is handled. Add other types as needed.
+  const webps = await glob('./assets/**/*.webp');
+  webps.forEach(async (webp) => {
+   const tsFilename = webp.replace(/webp$/, 'ts');
+   try {
+    // NOTE: This means that to update the image, the ts file will have to be deleted
+    access(tsFilename);
+   } catch (e) {
     console.log(e);
     const webpBuffer = await readFile(webp);
     const base64Webp = webpBuffer.toString('base64');
@@ -21,8 +26,8 @@ const processImages = async function processImages() {
     const tsFile = `export default '${dataUrl}';\n`;
     writeFile(tsFilename, tsFile);
    }
-  }
- });
-};
+  });
+ };
 
-processImages();
+ processImages();
+}
