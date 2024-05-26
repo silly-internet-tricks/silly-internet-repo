78f49c4aa1d6bd0fffb91b02afa068c5498f39be
Josh Parker <JoshWolfParker@gmail.com>
2024-05-26 16:22:08 -0600

    add the show twitch chat userscript (without external dependencies)

--- src/lib/util/insert-css.ts
+++ src/lib/util/insert-css.ts
@@ -1,7 +1,7 @@
 export default function insertCSS(css: string) {
  const style: HTMLStyleElement = document.createElement('style');
  style.appendChild(new Text(css));
- document.body.appendChild(style);
+ document.head.appendChild(style);
 
  return style.sheet;
 }
--- src/userscripts/apis/show-twitch-chat.user.ts
+++ src/userscripts/apis/show-twitch-chat.user.ts
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Toast Twitch Chat
+// @name         Show Twitch Chat
 // @namespace    http://tampermonkey.net/
 // @version      2024-04-22
-// @description  Show twitch chat no matter what website you're looking at (using react toastify)
+// @description  Show twitch chat no matter what website you're looking at (no external dependencies)
 // @author       Josh Parker
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikipedia.org
@@ -11,18 +11,39 @@
 // @source       https://github.com/silly-internet-tricks/silly-internet-repo/blob/main/src/userscripts/apis/toast-twitch-chat.user.ts
 // @downloadURL  https://gist.githubusercontent.com/silly-internet-tricks/9b6d4a20dcf00cddd53ac1392aabc5f0/raw/toast-twitch-chat.user.js
 // @updateURL    https://gist.githubusercontent.com/silly-internet-tricks/9b6d4a20dcf00cddd53ac1392aabc5f0/raw/toast-twitch-chat.meta.js
+// @noframes
 // ==/UserScript==
 
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import getStringFromChunk from '../../lib/util/get-string-from-chunk';
 import insertCSS from '../../lib/util/insert-css';
 import observeElementRemovalAndReaddIt from '../../lib/util/observe-element-removal-and-readd-it';
 
 (function subscribeTwitchChat() {
-  const emptyElementId = 'toast-twitch-chat-empty-element';
+ const showChat = (function makeShowChat() {
+  const chatModal = document.createElement('div');
+  const chatModalUl = document.createElement('ul');
+  chatModal.appendChild(chatModalUl);
+  let timeoutNumber: NodeJS.Timeout;
+  return function showChatFunction(chat: string) {
+   const chatLi = document.createElement('li');
+   chatLi.appendChild(new Text(chat));
+
+   chatModalUl.appendChild(chatLi);
+
+   document.body.appendChild(chatModal);
+   chatModal.classList.remove('twitch-chat-modal');
+   chatModal.classList.add('twitch-chat-modal');
+   clearTimeout(timeoutNumber);
+   timeoutNumber = setTimeout(() => {
+    chatModal.classList.remove('twitch-chat-modal');
+    chatModal.parentElement.removeChild(chatModal);
+   }, 5000);
+
+   setTimeout(() => chatLi.parentElement.removeChild(chatLi), 5000);
+  };
+ })();
+
+ const emptyElementId = 'toast-twitch-chat-empty-element';
 
  insertCSS(`
   .twitch-chat-toast {
@@ -32,23 +53,38 @@ import observeElementRemovalAndReaddIt from '../../lib/util/observe-element-remo
   #${emptyElementId} {
     z-index: 9001;
   }
-  `);
 
- const App = function App() {
-  return (
-   <div id="toast-twitch-chat-userscript">
-    <ToastContainer />
-   </div>
-  );
- };
+  .twitch-chat-modal {
+   position: fixed;
+   top: 2px;
+   left: 2px;
+   font-size: 2rem;
+   z-index: 102;
+   background-image: linear-gradient(to top, chartreuse 0%, yellow 100%);
+   padding: 0.5rem;
+   box-shadow: 2px 2px 2px black;
+   text-shadow: 1px 1px 1px magenta;
+   animation-name: modal-fade-out;
+   animation-duration: 5s;
+   animation-timing-function: linear;
+   animation-iteration-count: 1;
+}
+
+@keyframes modal-fade-out {
+ 0% {
+  opacity: 100%;
+ }
+
+ 100% {
+  opacity: 0%;
+ }
+}
+  `);
 
  const emptyElement = document.createElement('div');
  emptyElement.id = emptyElementId;
  document.body.appendChild(emptyElement);
  observeElementRemovalAndReaddIt(emptyElement);
- const root = createRoot(emptyElement);
-
- root.render(<App />);
 
  const requestOptions: GmXmlHttpRequestRequestOptions = {
   url: 'http://localhost:9821',
@@ -58,9 +94,7 @@ import observeElementRemovalAndReaddIt from '../../lib/util/observe-element-remo
    for await (const chunk of response) {
     const chunkString = getStringFromChunk(chunk);
     console.log(chunkString);
-    toast(chunkString, {
-     className: 'twitch-chat-toast',
-    });
+    showChat(chunkString);
    }
   },
  };
